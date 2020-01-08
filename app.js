b09b0459586c80a38472abab3d6f40b3a6a1fb87
Adam Beck <Adam@codelobe.io>
2020-01-08 09:01:35 -0500

    Add annotations

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import * as greet from "./greet.js";
-import math from "./common/math.js";
+import math from "./common/math.js"; // this module is written with CommonJS syntax; it can be stubbed
 import sinon from "sinon";
 
 const friend = "World";
 
 sinon.stub(math, "add").returns(50);
+
+// NOTE: Cannot stub ES Modules and their methods. Sinon recognizes this and throws an error.
 // sinon.stub(greet, "simple").returns(() => "FOO!");
 
 function sayHelloToFriend(friend) {
